1b2ba14b31586e31273202f2d89eca129f2066f1
Pedro <pedroduran014@gmail.com>
2021-11-07 22:57:59 -0500

    correccion en el provider de redux

--- actions/auth.js
+++ actions/auth.js
@@ -1,3 +1,26 @@
+import firebaseApp from "../firebase/firebase-config";
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+import { types } from "../types/types";
+import { finishLoading, startLoading } from "./ui";
+
+const auth = getAuth(firebaseApp);
+
+export const startLoginEmailPassword = (email, password) => {
+  return (dispatch) => {
+    dispatch(startLoading());
+    signInWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        dispatch(finishLoading());
+        dispatch(login(user.uid, user.displayName));
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(finishLoading());
+      });
+  };
+};
+
 export const login = (uid, displayName) => ({
   type: types.login,
   payload: {
@@ -5,6 +28,13 @@ export const login = (uid, displayName) => ({
     displayName,
   },
 });
+
+export const startLogout = () => {
+  return async (dispatch) => {
+    await signOut(auth);
+    dispatch(logout());
+  };
+};
 
 export const logout = () => ({
   type: types.logout,
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import "../styles/globals.css";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </Provider>
+  );
 }
 
 export default MyApp;
